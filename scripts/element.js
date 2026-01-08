c3c0374047f4070dadf57e203e42e022f5709777
evoexo <dragon520111back@gmail.com>
2026-01-08 12:17:20 -0500

    Added data persistence for element unlocks using local storage.

--- scripts/element.js
+++ scripts/element.js
@@ -1,9 +1,10 @@
 import fusions from "./fusions.js";
 import Storable from "./store.js";
 
-const ELEMENT_DIV_EVENTS = Object.freeze({
+const ELEMENT_EVENTS = Object.freeze({
     ADDED : 0,
-    CLICKED : 1
+    CLICKED : 1,
+    INITIALIZED: 2
 });
 
 const ELEMENT_ICON_MAP =  new Map();
@@ -62,6 +63,7 @@ class ElementSubject{
     }
 
     subscribe(observer){
+        this.notify(null, ELEMENT_EVENTS.INITIALIZED);
         this.observers.push(observer);
     }
 
@@ -79,13 +81,14 @@ class ElementManager extends ElementSubject{
         super();
         this.#elementContainerDiv = elementContainerDiv;
         this.#elements = [];
+        this.notify(null, ELEMENT_EVENTS.INITIALIZED);
     }
 
     addElement(element){
         this.#elementContainerDiv.appendChild(element.getDiv());
         this.#elements.push(element);
-        element.getDiv().addEventListener("click", () => this.notify(element, ELEMENT_DIV_EVENTS.CLICKED));
-        this.notify(element, ELEMENT_DIV_EVENTS.ADDED);
+        element.getDiv().addEventListener("click", () => this.notify(element, ELEMENT_EVENTS.CLICKED));
+        this.notify(element, ELEMENT_EVENTS.ADDED);
     }
 
     removeElement(element) {
@@ -119,12 +122,17 @@ class ElementHistory extends Storable{
         super("elementHistory");
         this.#elementUnlocksDiv = elementUnlocksDiv;
         this.#elementUnlocksCounterDiv = elementUnlocksCounterDiv;
-        this.#seenElements = new Set();
+        this.#seenElements = new Set(JSON.parse(this.getValue()));
     }
-
     // Observer method
     update(subject, element, eventType){
-        if (eventType != ELEMENT_DIV_EVENTS.ADDED){
+        if(eventType === ELEMENT_EVENTS.INITIALIZED){
+            this.#initializeUnlocksDiv(subject);
+            this.#updateUnlocksCounterDiv();
+            return;
+        }
+
+        if (eventType != ELEMENT_EVENTS.ADDED){
             return;
         }
 
@@ -134,11 +142,25 @@ class ElementHistory extends Storable{
         }
 
         this.#seenElements.add(elementName);
+        this.#addElementToUnlocksDiv(subject, element);
+        this.#updateUnlocksCounterDiv();
+        this.loadValue(this.#seenElements);
+    }
+    
+    #initializeUnlocksDiv(subject) {
+        for (const elementName of this.#seenElements) {
+            const element = new Element(elementName);
+            this.#addElementToUnlocksDiv(subject, element);
+        }
+    }
 
+    #addElementToUnlocksDiv(subject, element){
         const elementDiv = element.getClone().getDiv();
-        elementDiv.addEventListener("click", () => {subject.addElement(element.getClone())});
-
         this.#elementUnlocksDiv.appendChild(elementDiv);
+        elementDiv.addEventListener("click", () => {subject.addElement(element.getClone())});
+    }
+
+    #updateUnlocksCounterDiv(){
         this.#elementUnlocksCounterDiv.innerHTML = `${this.getNumElementsSeen()}/${TOTAL_ELEMENTS}`;
     }
 
@@ -184,7 +206,7 @@ class ElementFuser {
 
     // Observer method
     update(subject, element, eventType){
-        if (eventType != ELEMENT_DIV_EVENTS.CLICKED){
+        if (eventType != ELEMENT_EVENTS.CLICKED){
             return;
         }
         
