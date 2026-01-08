3e959a9338ef1f6b0bbaab734943740c5ef79149
evoexo <dragon520111back@gmail.com>
2026-01-08 10:57:39 -0500

    Added project files

---
+++ scripts/element.js
@@ -0,0 +1,204 @@
+import fusions from "./fusions.js";
+
+const ELEMENT_DIV_EVENTS = Object.freeze({
+    ADDED : 0,
+    CLICKED : 1
+});
+
+const ELEMENT_ICON_MAP =  new Map();
+for (const [element,,] of fusions){
+    if (!ELEMENT_ICON_MAP.has(element)){
+        ELEMENT_ICON_MAP.set(element, `../icons/${element}.png`);
+    }
+}
+
+const TOTAL_ELEMENTS = ELEMENT_ICON_MAP.size;
+
+class Element{
+    constructor(name){
+        this.name = name;
+        this.div = this.#createDiv();
+    }
+
+    #createDiv() {
+        const div = document.createElement("div");
+        div.className = "elementContainerItem textFormat";
+        div.innerHTML = `
+            <img src="${ELEMENT_ICON_MAP.get(this.name)}">
+            <p>${this.name}</p>
+        `;
+        return div;
+    }
+
+    getDiv(){
+        return this.div;
+    }
+
+    getClone(){
+        return new Element(this.getName());
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    addClass(className){
+        this.div.classList.add(className);
+    }
+
+    removeClass(className){
+        this.div.classList.remove(className);
+    }
+}
+
+class ElementSubject{
+    constructor(){
+        this.observers = [];
+    }
+
+    notify(element, eventType){
+        this.observers.forEach((observer) => observer.update(this, element, eventType));
+    }
+
+    subscribe(observer){
+        this.observers.push(observer);
+    }
+
+    unsubscribe(observer){
+        this.observers = this.observers.filter((ob) => ob != observer);
+    }
+}
+
+// Container for elements; Concrete Subject
+class ElementManager extends ElementSubject{
+    #elementContainerDiv;
+    #elements;
+
+    constructor(elementContainerDiv){
+        super();
+        this.#elementContainerDiv = elementContainerDiv;
+        this.#elements = [];
+    }
+
+    addElement(element){
+        this.#elementContainerDiv.appendChild(element.getDiv());
+        this.#elements.push(element);
+        element.getDiv().addEventListener("click", () => this.notify(element, ELEMENT_DIV_EVENTS.CLICKED));
+        this.notify(element, ELEMENT_DIV_EVENTS.ADDED);
+    }
+
+    removeElement(element) {
+        const index = this.#elements.indexOf(element);
+        if (index === -1){
+            return false;
+        }
+
+        element.getDiv().remove();
+        this.#elements.splice(index, 1);
+        return true;
+    }
+
+    removeAllElements(){
+        this.#elements.length = 0;
+        this.#elementContainerDiv.innerHTML = "";
+    }
+
+
+    getElements(){
+        return this.#elements;
+    }
+}
+
+class ElementHistory{
+    #seenElements;
+    #elementUnlocksDiv;
+    #elementUnlocksCounterDiv;
+
+    constructor(elementUnlocksDiv, elementUnlocksCounterDiv){
+        this.#elementUnlocksDiv = elementUnlocksDiv;
+        this.#elementUnlocksCounterDiv = elementUnlocksCounterDiv;
+        this.#seenElements = new Set();
+    }
+
+    getNumElementsSeen(){
+        return this.#seenElements.size;
+    }
+
+    // Observer method
+    update(subject, element, eventType){
+        if (eventType != ELEMENT_DIV_EVENTS.ADDED){
+            return;
+        }
+
+       const elementName = element.getName();
+       if (this.#seenElements.has(elementName)){
+            return;
+       }
+
+       this.#seenElements.add(elementName);
+       // Need to clone element because passed reference
+       const elementDiv = element.getClone().getDiv();
+       this.#elementUnlocksDiv.appendChild(elementDiv);
+       elementDiv.addEventListener("click", () => {subject.addElement(element.getClone())});
+       
+       this.#elementUnlocksCounterDiv.innerHTML = `${this.getNumElementsSeen()}/${TOTAL_ELEMENTS}`;
+    }
+}
+
+class ElementFuser {
+    #fusionMap;
+    #lastClickedElement;
+
+    constructor() {
+        this.#fusionMap = new Map();
+        this.#initializeFusionMap();
+        this.#lastClickedElement = null;
+    }
+
+    #addFusion(elementOneName, elementTwoName, result) {
+        this.#addElementIfAbsent(elementOneName).set(elementTwoName, result);
+        this.#addElementIfAbsent(elementTwoName).set(elementOneName, result);
+    }
+
+    #addElementIfAbsent(elementName) {
+        if (!this.#fusionMap.has(elementName)) {
+            this.#fusionMap.set(elementName, new Map());
+        }
+        return this.#fusionMap.get(elementName);
+    }
+
+    #initializeFusionMap() {
+        fusions.forEach(([result, elementOneName, elementTwoName]) => this.#addFusion(elementOneName, elementTwoName, result));
+    }
+
+    getFusionResult(elementOne, elementTwo) {
+        const resultName = this.#fusionMap.get(elementOne.getName())?.get(elementTwo.getName());
+        return resultName ? new Element(resultName) : null;
+    }
+
+    // Observer method
+    update(subject, element, eventType){
+        if (eventType != ELEMENT_DIV_EVENTS.CLICKED){
+            return;
+        }
+        
+        if (!this.#lastClickedElement){
+            element.addClass("selected");
+            this.#lastClickedElement = element;
+            return;
+        }
+
+        const fusionResult = this.getFusionResult(element, this.#lastClickedElement);
+        if (fusionResult){
+            subject.removeElement(element);
+            subject.removeElement(this.#lastClickedElement);
+            subject.addElement(fusionResult);
+        }
+
+        this.#lastClickedElement.removeClass("selected");
+        this.#lastClickedElement = null;
+    }
+}
+
+
+export {ElementManager, Element, ElementHistory, ElementFuser};
---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+import {ElementManager, Element, ElementHistory, ElementFuser} from "./element.js";
+const startingElements = [
+    new Element("water"),
+    new Element("fire"),
+    new Element("earth"),
+    new Element("air"),
+]
+
+const elementUnlocksDiv = document.getElementsByClassName("elementUnlocks")[0];
+const elementUnlocksCounterDiv = document.getElementsByClassName("elementUnlocksCounter")[0];
+const elementContainerDiv = document.getElementsByClassName("elementContainer")[0];
+const elementClearDiv = document.getElementById("elementClear");
+
+const elementHistory = new ElementHistory(elementUnlocksDiv, elementUnlocksCounterDiv);
+const elementFuser = new ElementFuser();
+
+const elementManager = new ElementManager(elementContainerDiv);
+
+elementClearDiv.addEventListener("click", () => elementManager.removeAllElements());
+elementManager.subscribe(elementHistory);
+elementManager.subscribe(elementFuser);
+
+startingElements.forEach((element) => elementManager.addElement(element));
